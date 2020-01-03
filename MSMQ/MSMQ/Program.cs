779039ce7cc3f7f9e38b22e3467d60a2ae6892e8
Saurabh Navdkar <saurabh.navdkar@gmail.com>
2020-01-03 19:28:39 +0530

    msmq changes

--- MSMQ/MSMQ/Program.cs
+++ MSMQ/MSMQ/Program.cs
@@ -10,6 +10,8 @@ namespace MSMQ
     using Experimental.System.Messaging;
     using System.Net;
     using System.Net.Mail;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Linq;
 
     /// <summary>
     /// this is class program
@@ -23,26 +25,34 @@ namespace MSMQ
         static void Main(string[] args)
         {
             MessageQueue msmq = new MessageQueue();
-       
-                msmq.Path = @".\private$\ForgotPassword";
-                Message message = new Message();
-                message = msmq.Receive(new TimeSpan(0,0,5));
-                message.Formatter = new XmlMessageFormatter(new string[] { "System.String,mscorlib" });
-                string msmqMessage = message.Body.ToString();
-                Console.WriteLine(msmqMessage);
+            string baseUrl = "http://localhost:3000/reset/";
+            msmq.Path = @".\private$\ForgotPassword";
+            Message message = new Message();
+            message = msmq.Receive(new TimeSpan(0,0,5));
+            message.Formatter = new XmlMessageFormatter(new string[] { "System.String,mscorlib" });
+            string msmqMessage = message.Body.ToString();
+            Console.WriteLine(msmqMessage);
 
-                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
-                client.EnableSsl = true;
-                client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                client.UseDefaultCredentials = false;
-                client.Credentials = new NetworkCredential("saurabhnavdkar112@gmail.com", "saurabh@123");
-                MailMessage mailMessage = new MailMessage();
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("saurabhnavdkar112@gmail.com", "saurabh@123");
+            MailMessage mailMessage = new MailMessage();
             mailMessage.To.Add("saurabhnavdkar112@gmail.com");
             mailMessage.From = new MailAddress("saurabhnavdkar112@gmail.com");
-                mailMessage.Subject = "Forgot Password conformation";
-                mailMessage.Body = msmqMessage;
+            mailMessage.Subject = "Forgot Password conformation";
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Body = "<h4><a href="+baseUrl+ msmqMessage+"/>click here</a><br></br>to reset password</h4>";
             client.Send(mailMessage);
-            Console.WriteLine("message sent");
+
+            var stream = msmqMessage;
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(stream);
+            var tokenS = handler.ReadToken(stream) as JwtSecurityToken;
+            string Email = tokenS.Claims.FirstOrDefault(claim => claim.Type == "email").Value;
+            //int Id = tokenS.Claims.FirstOrDefault(claim => claim.Type == "id").Value;
+            Console.WriteLine("message sent to "+Email);
             Console.ReadKey();
         }
     }
